847d0ea2e66242d240a55ce71f656f0fc89e0783
Kevin <45508696+Kevin0412@users.noreply.github.com>
2023-09-01 21:41:14 +0800

    Create 7_2.cpp

---
+++ 7_2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string name;
+    cout<<"Hello!What's your name?"<<endl;
+    cin>>name;
+    cout<<name<<",Welcome to learn OOP using C++!"<<endl;
+}
