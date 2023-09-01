5d34d56fdca21b1716237aba18ef25b6eb4fc171
Kevin <45508696+Kevin0412@users.noreply.github.com>
2023-09-01 21:42:42 +0800

    Create 7_6.c

---
+++ 7_6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,c,d;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    if(b<d){
+        printf("%d:%d",c-a,d-b);
+    }else{
+        printf("%d:%d",c-a-1,d-b+60);
+    }
+}
